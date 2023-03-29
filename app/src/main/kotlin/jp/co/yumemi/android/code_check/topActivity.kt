425867d7d9415eee800f1fc35aa6b5c6abeddc2c
LottieAnne <kkang_chung125@yahoo.co.jp>
2023-03-29 11:08:08 +0900

    feat: Composeを利用したNavigaiton

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/topActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/topActivity.kt
@@ -3,8 +3,38 @@
  */
 package jp.co.yumemi.android.code_check
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import jp.co.yumemi.android.code_check.domain.RepositoryInfo
+import jp.co.yumemi.android.code_check.repositoryDetail.RepositoryDetailArg
+import jp.co.yumemi.android.code_check.repositoryDetail.RepositoryDetailRoute
+import jp.co.yumemi.android.code_check.repositoryList.RepositoryListRoute
 
 @AndroidEntryPoint
-class TopActivity : AppCompatActivity(R.layout.activity_top)
+class TopActivity : AppCompatActivity(R.layout.activity_top) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "list") {
+                composable("list") {
+                    RepositoryListRoute(
+                        viewModel = hiltViewModel(),
+                        onRepositoryClick = {navController.navigate("detail")}
+                    )
+                }
+                composable("detail") {
+                    val args = it.arguments?.getParcelable("key", RepositoryDetailArg::class.java)
+                    RepositoryDetailRoute(repositoryInfo = checkNotNull(args?.repositoryInfo))
+                }
+            }
+        }
+    }
+}
